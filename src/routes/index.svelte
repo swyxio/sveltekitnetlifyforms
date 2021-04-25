47fe25f9b27e1d512757d5bb36a732dbfdfb1d76
sw-yx <shawnthe1@gmail.com>
2021-04-26 05:24:14 +0800

    submission

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,9 +6,9 @@
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <!-- "/src/routes/test-form.svelte" -->
-<form name="test" method="post" action="/success">
+<form name="test" method="post" netlify>
     <input type="hidden" name="form-name" value="test" />
-    <input type="text" name="bot-field" />
+    <!-- <input type="text" name="bot-field" /> -->
     <p>
         <label>Your Name: <input type="text" name="name" /></label>
     </p>
