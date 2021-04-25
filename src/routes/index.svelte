39b320b9a449dbe7aea0e07b9d914f90e3f6a2be
sw-yx <shawnthe1@gmail.com>
2021-04-26 04:24:40 +0800

    init

---
+++ src/routes/index.svelte
@@ -0,0 +1,20 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<!-- "/src/routes/test-form.svelte" -->
+<form name="test" method="post" action="/success">
+    <input type="hidden" name="form-name" value="test" />
+    <input type="text" name="bot-field" />
+    <p>
+        <label>Your Name: <input type="text" name="name" /></label>
+    </p>
+    <p>
+        <label>Your Email: <input type="email" name="email" /></label>
+    </p>
+    <p>
+        <label>Message: <textarea name="message" /></label>
+    </p>
+    <p>
+        <button type="submit">Send</button>
+    </p>
+</form>
