2df9e504d8023a44f76a8fa6e1930387453ccfc6
sw-yx <shawnthe1@gmail.com>
2021-04-26 05:04:27 +0800

    netlify adapter

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,5 +1,8 @@
 /** @type {import('@sveltejs/kit').Config} */
+const adapter = require('@sveltejs/adapter-netlify');
 module.exports = {
+
+	adapter: adapter(''),
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
